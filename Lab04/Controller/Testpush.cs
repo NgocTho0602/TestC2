1452894d02aaef135e136ebfa643a16e1fd37af6
Ngoc Tho <ngoctho@gmail.com>
2023-12-11 16:29:00 +0700

    Create main

--- Lab04/Controller/Testpush.cs
+++ Lab04/Controller/Testpush.cs
@@ -1,4 +1,4 @@
-﻿gitusing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lab04.Controller
 {
-    internal class Login
+    internal class Testpush
     {
     }
 }
